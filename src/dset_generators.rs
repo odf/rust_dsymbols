ab8470a935492b178d37aa778665a3ae868a907b
odf <olaf.delgado@gmail.com>
2023-01-11 19:49:10 +1100

    More work on dset generator

--- src/dset_generators.rs
+++ src/dset_generators.rs
@@ -51,7 +51,7 @@ impl BackTracking for DSetBackTracking {
 
                     dset.set(i, d, e);
 
-                    let (head, tail, gap, k) = scan02Orbit(&dset, d);
+                    let (head, tail, gap, k) = scan_02_orbit(&dset, d);
 
                     if gap == 1 {
                         dset.set(k, head, tail);
@@ -59,7 +59,7 @@ impl BackTracking for DSetBackTracking {
                         continue;
                     }
 
-                    if check_canonicity(&dset, &is_remap_start) {
+                    if check_canonicity(&dset, &mut is_remap_start) {
                         result.push(Self::State { dset, is_remap_start });
                     }
                 }
@@ -67,6 +67,23 @@ impl BackTracking for DSetBackTracking {
         }
 
         result
+    }
+}
+
+
+pub struct DSets(BackTrackIterator<DSetBackTracking>);
+
+impl DSets {
+    pub fn new(dim: usize, max_size: usize) -> DSets {
+        DSets(BackTrackIterator::new(DSetBackTracking { dim, max_size }))
+    }
+}
+
+impl Iterator for DSets {
+    type Item = SimpleDSet;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.next()
     }
 }
 
@@ -83,11 +100,66 @@ fn first_undefined(ds: &PartialDSet) -> Option<(usize, usize)> {
 }
 
 
-fn scan02Orbit(ds: &PartialDSet, d: usize) -> (usize, usize, usize, usize) {
-    todo!()
+fn scan_02_orbit(ds: &PartialDSet, d: usize) -> (usize, usize, usize, usize) {
+    let (head, i) = scan(ds, [0, 2, 0, 2], d, 4);
+    let (tail, j) = scan(ds, [2, 0, 2, 0], d, 4 - i);
+
+    (head, tail, 4 - i - j, 2 * (i % 2))
 }
 
 
-fn check_canonicity(ds: &PartialDSet, is_remap_start: &Vec<bool>) -> bool {
-    todo!()
+fn scan(
+    ds: &PartialDSet, w: [usize; 4], d: usize, limit: usize
+) -> (usize, usize)
+{
+    let mut e = d;
+
+    for k in 1..=limit {
+        if let Some(en) = ds.get(w[k], e) {
+            e = en;
+        } else {
+            return (e, k - 1);
+        }
+    }
+
+    (e, limit)
+}
+
+
+fn check_canonicity(
+    ds: &PartialDSet, is_remap_start: &mut [bool]
+) -> bool
+{
+    let mut n2o = Vec::with_capacity(ds.size() + 1);
+    let mut o2n = Vec::with_capacity(ds.size() + 1);
+
+    for d in 1..=ds.size() {
+        if is_remap_start[d] {
+            let diff = compare_renumbered_from(ds, d, &mut n2o, &mut o2n);
+
+            if diff < 0 {
+                return false;
+            } else {
+                is_remap_start[d] = false;
+            }
+        }
+    }
+
+    true
+}
+
+
+fn compare_renumbered_from(
+    ds: &PartialDSet, d0: usize, n2o: &mut [usize], o2n: &mut [usize]
+) -> i64
+{
+    n2o.fill(0);
+    o2n.fill(0);
+
+    n2o[1] = d0;
+    o2n[d0] = 1;
+
+    let mut next = 2;
+
+    0
 }
