7d5ed22ed555c500d32fc4bac35d3f8743f084a7
odf <olaf.delgado@gmail.com>
2023-01-15 16:00:56 +1100

    finish first version of DSym generator (needs more testing)

--- src/dsym_generators.rs
+++ src/dsym_generators.rs
@@ -1,4 +1,5 @@
 use num_rational::Rational64;
+use num_traits::signum;
 
 use crate::backtrack::BackTrackIterator;
 use crate::backtrack::BackTracking;
@@ -16,9 +17,9 @@ struct DSymGenState {
 struct DSymBackTracking {
     dset: SimpleDSet,
     orbit_index: Vec<Vec<usize>>,
-    orbit_rs: Vec<usize>,
     orbit_vmins: Vec<usize>,
     orbit_is_chain: Vec<bool>,
+    orbit_maps: Vec<Vec<usize>>,
 }
 
 impl DSymBackTracking {
@@ -43,30 +44,209 @@ impl DSymBackTracking {
             }
         }
 
+        let mut orbit_maps =vec![];
+        for map in dset.automorphisms() {
+            let mut m = vec![0; orbit_rs.len()];
+            for i in 0..dset.dim() {
+                for d in 1..=dset.size() {
+                    m[orbit_index[i][d]] = orbit_index[i][map[d]];
+                }
+            }
+            orbit_maps.push(m);
+        }
+
         DSymBackTracking {
             dset: dset.clone(),
             orbit_index,
-            orbit_rs,
             orbit_vmins,
             orbit_is_chain,
+            orbit_maps,
         }
     }
+
+    fn orbit_count(&self) -> usize {
+        self.orbit_vmins.len()
+    }
+
+    fn curvature(&self, vs: &[usize]) -> Rational64 {
+        let n = self.dset.size() as i64;
+        let mut curv = Rational64::new(-n, 2);
+
+        for i in 0..self.orbit_count() {
+            let k = if self.orbit_is_chain[i] { 1 } else { 2 };
+            curv += Rational64::new(k, vs[i] as i64);
+        }
+
+        curv
+    }
+
+    fn is_minimally_hyperbolic(&self, vs: &[usize], curv: Rational64) -> bool {
+        if signum(curv).to_integer() >= 0 {
+            false
+        } else {
+            for i in 0..self.orbit_count() {
+                if vs[i] > self.orbit_vmins[i] {
+                    let v = vs[i] as i64;
+                    let k = if self.orbit_is_chain[i] { 1 } else { 2 };
+                    let c = curv
+                        - Rational64::new(k, v)
+                        + Rational64::new(k, v - 1);
+                    if signum(c).to_integer() < 0 {
+                        return false;
+                    }
+                }
+            }
+            true
+        }
+    }
+
+    fn is_canonical(&self, vs: &[usize]) -> bool {
+        for m in &self.orbit_maps {
+            let ws: Vec<_> = (0..vs.len()).map(|i| vs[m[i]]).collect();
+            if &ws[..] > vs {
+                return false;
+            }
+        }
+        true
+    }
+
+    fn is_good(&self, vs: &[usize], curv: Rational64) -> bool {
+        if signum(curv).to_integer() <= 0 {
+            true
+        } else {
+            let ds = &self.dset;
+            let mut cones = vec![];
+            let mut corners = vec![];
+
+            for d in ds.orbit_reps_2d(0, 2) {
+                let d0 = ds.get(0, d).unwrap();
+                let d2 = ds.get(2, d).unwrap();
+
+                if d0 == d && d2 == d {
+                    corners.push(2)
+                } else if d0 != d && d2 == d0 {
+                    cones.push(2)
+                }
+            }
+
+            for i in 0..self.orbit_count() {
+                if vs[i] > 1 {
+                    if self.orbit_is_chain[i] {
+                        corners.push(vs[i]);
+                    } else {
+                        cones.push(vs[i]);
+                    }
+                }
+            }
+
+            cones.sort();
+            cones.reverse();
+            corners.sort();
+            corners.reverse();
+
+            let front = cones.iter()
+                .map(|v| v.to_string())
+                .collect::<Vec<_>>()
+                .join("");
+            let middle = String::from(
+                if self.dset.is_loopless() { "" } else { "*" }
+            );
+            let back = corners.iter()
+                .map(|v| v.to_string())
+                .collect::<Vec<_>>()
+                .join("");
+            let cross = String::from(
+                if self.dset.is_weakly_oriented() { "" } else { "x" }
+            );
+            let key = [ front, middle, back, cross ].join("");
+
+            [
+                "", "*", "x",
+                "532", "432", "332",
+                "422", "322", "222",
+                "44", "33", "22",
+                "*532", "*432", "*332", "3*2",
+                "*422", "*322", "*222", "2*4", "2*3", "2*2",
+                "*44", "*33", "*22", "4*", "3*", "2*", "4x", "3x", "2x"
+            ].contains(&&key[..])
+        }
+    }
+
+    fn make_dsym(&self, vs: &[usize]) -> PartialDSym {
+        let mut ds = PartialDSym::new(&self.dset);
+
+        for i in 0..ds.dim() {
+            for d in 1..=ds.size() {
+                ds.set_v(i, d, vs[self.orbit_index[i][d]]);
+            }
+        }
+
+        ds
+    }
 }
+
 
 impl BackTracking for DSymBackTracking {
     type State = DSymGenState;
     type Item = PartialDSym;
 
     fn root(&self) -> Self::State {
-        todo!()
+        let vs = self.orbit_vmins.clone();
+        let curv = self.curvature(&vs);
+        let next = 0;
+
+        Self::State { vs, curv, next }
     }
 
     fn extract(&self, state: &Self::State) -> Option<Self::Item> {
-        todo!()
+        if state.next >= self.orbit_count()
+            && self.is_good(&state.vs, state.curv)
+            && self.is_canonical(&state.vs)
+        {
+            Some(self.make_dsym(&state.vs))
+        } else {
+            None
+        }
     }
 
     fn children(&self, state: &Self::State) -> Vec<Self::State> {
-        todo!()
+        let mut result = vec![];
+
+        if state.next < self.orbit_count() {
+            if signum(state.curv).to_integer() < 0 {
+                result.push(Self::State {
+                    vs: state.vs.clone(),
+                    curv: state.curv,
+                    next: self.orbit_count(),
+                })
+            } else {
+                let n = state.next;
+                let vmin = state.vs[n];
+
+                for v in vmin..=7 {
+                    let mut vs = state.vs.clone();
+                    vs[n] = v;
+
+                    let k = if self.orbit_is_chain[n] { 1 } else { 2 };
+                    let curv = state.curv
+                        - Rational64::new(k, vmin as i64)
+                        + Rational64::new(k, v as i64);
+
+                    if signum(curv).to_integer() >= 0
+                        || self.is_minimally_hyperbolic(&vs, curv)
+                    {
+                        let next = state.next + 1;
+                        result.push(Self::State { vs, curv, next });
+                    }
+
+                    if signum(curv).to_integer() < 0 {
+                        break;
+                    }
+                }
+            }
+        }
+
+        result
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use rust_dsymbols::dset_generators::DSets;
-use rust_dsymbols::dsets::{DSet, OrientedCover};
-use rust_dsymbols::dsyms::PartialDSym;
+use rust_dsymbols::dsym_generators::DSyms;
 
 
 fn main() {
@@ -9,9 +8,9 @@ fn main() {
     if let Some(arg) = std::env::args().nth(1) {
         let n: usize = arg.parse().unwrap();
 
-        for ds in DSets::new(2, n) {
-            if ds.size() == n {
-                println!("{}", ds);
+        for dset in DSets::new(2, n) {
+            for dsym in DSyms::new(&dset) {
+                println!("{}", dsym);
                 count += 1;
             }
         }
@@ -20,23 +19,6 @@ fn main() {
         println!();
     } else {
         panic!("Expected an argument.");
-    }
-
-    count = 0;
-    for ds in DSets::new(2, 4) {
-        count += 1;
-        if count == 25 {
-            let mut ds = PartialDSym::new(&ds);
-            ds.set_v(0, 1, 2);
-            ds.set_v(1, 1, 3);
-            ds.set_v(1, 2, 4);
-            println!("  Example symbol: {}", &ds);
-            let cov = ds.oriented_cover().unwrap();
-            println!("  Oriented cover: {}", &cov);
-            println!("  Oriented cover is oriented: {}", cov.is_oriented());
-            let map = cov.morphism(&ds, 1);
-            println!("  Covering map: {:?}", &map);
-        }
     }
     println!();
 }
