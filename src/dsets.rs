bc7c8be4b7be71f1192ac67c9cc4da8bf76e320d
odf <olaf.delgado@gmail.com>
2023-01-16 11:41:01 +1100

    speed up DSet/DSym formatting

--- src/dsets.rs
+++ src/dsets.rs
@@ -162,47 +162,67 @@ pub trait DSet {
 
 
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let mut chunks = vec!();
+        let mut text = String::with_capacity(self.dim() * self.size() * 2);
 
-        chunks.push(format!("<{}.{}:", self.set_count(), self.symbol_count()));
+        text.push('<');
+        string_push_num(&mut text, self.set_count());
+        text.push('.');
+        string_push_num(&mut text, self.symbol_count());
+        text.push(':');
 
+        string_push_num(&mut text, self.size());
         if self.dim() != 2 {
-            chunks.push(format!("{} {}:", self.size(), self.dim()));
-        } else {
-            chunks.push(format!("{}:", self.size()));
+            text.push(' ');
+            string_push_num(&mut text, self.dim());
         }
+        text.push(':');
 
         for i in 0..=self.dim() {
             if i > 0 {
-                chunks.push(",".to_string());
+                text.push(',');
             }
             for d in 1..=self.size() {
                 let e = self.get(i, d).unwrap_or(0);
                 if e == 0 || e >= d {
                     if d > 1 {
-                        chunks.push(" ".to_string());
+                        text.push(' ');
                     }
-                    chunks.push(e.to_string());
+                    string_push_num(&mut text, e);
                 }
             }
         }
-        chunks.push(":".to_string());
+        text.push(':');
 
         for i in 0..self.dim() {
             if i > 0 {
-                chunks.push(",".to_string());
+                text.push(',');
             }
 
             for d in self.orbit_reps_2d(i, i + 1) {
                 if d > 1 {
-                    chunks.push(" ".to_string());
+                    text.push(' ');
                 }
-                chunks.push(self.m(i, i + 1, d).to_string());
+                string_push_num(&mut text, self.m(i, i + 1, d));
             }
         }
-        chunks.push(">".to_string());
+        text.push('>');
 
-        write!(f, "{}", chunks.join(""))
+        f.write_str(&text[..])
+    }
+}
+
+
+fn string_push_num(text: &mut String, n: usize) {
+    if n >= 1000 {
+        text.push_str(&n.to_string()[..]);
+    } else {
+        if n >= 100 {
+            text.push(char::from_digit((n / 100) as u32, 10).unwrap());
+        }
+        if n >= 10 {
+            text.push(char::from_digit(((n % 100) / 10) as u32, 10).unwrap());
+        }
+        text.push(char::from_digit((n % 10) as u32, 10).unwrap());
     }
 }
 
