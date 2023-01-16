a31d120214d7cd07d3f2891465d2e90b051f72df
odf <olaf.delgado@gmail.com>
2023-01-16 12:51:24 +1100

    make test executable produce compressed binary output

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,37 @@
+use std::str;
+
 use rust_dsymbols::dset_generators::DSets;
+use rust_dsymbols::dsets::DSet;
 use rust_dsymbols::dsym_generators::DSyms;
 
 
 fn main() {
-    let mut count = 0;
-
     if let Some(arg) = std::env::args().nth(1) {
         let n: usize = arg.parse().unwrap();
+        let mut previous = vec![];
 
         for dset in DSets::new(2, n) {
             for dsym in DSyms::new(&dset) {
-                println!("{}", dsym);
-                count += 1;
+                let code = dsym.to_binary().unwrap();
+
+                for i in 0..=code.len() {
+                    if i >= code.len()
+                        || i >= previous.len()
+                        || code[i] != previous[i]
+                    {
+                        print!(
+                            "{}{}",
+                            str::from_utf8(&[(i + 1) as u8]).unwrap(),
+                            str::from_utf8(&code[i..]).unwrap()
+                        );
+
+                        previous = code;
+                        break;
+                    }
+                }
             }
         }
-
-        println!("Found {} in total.", count);
-        println!();
     } else {
         panic!("Expected an argument.");
     }
-    println!();
 }
