c8d920b3d4d4321673fa4768e6b5d44b5a6d5196
odf <olaf.delgado@gmail.com>
2023-01-17 12:44:16 +1100

    move top level generation code into a function

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,47 @@
 use std::str;
 
 use rust_dsymbols::dset_generators::DSets;
-use rust_dsymbols::dsets::DSet;
+use rust_dsymbols::dsyms::DSym;
 use rust_dsymbols::dsym_generators::DSyms;
 
 
 fn main() {
     if let Some(arg) = std::env::args().nth(1) {
         let n: usize = arg.parse().unwrap();
-        let mut previous = vec![];
 
-        for dset in DSets::new(2, n) {
-            for dsym in DSyms::new(&dset) {
-                let code = dsym.to_binary().unwrap();
-
-                for i in 0..=code.len() {
-                    if i >= code.len()
-                        || i >= previous.len()
-                        || code[i] != previous[i]
-                    {
-                        print!(
-                            "{}{}",
-                            str::from_utf8(&[(i + 1) as u8]).unwrap(),
-                            str::from_utf8(&code[i..]).unwrap()
-                        );
-
-                        previous = code;
-                        break;
-                    }
-                }
-            }
-        }
+        generate_binary(n);
     } else {
         panic!("Expected an argument.");
     }
+}
+
+
+fn generate_binary(n: usize) {
+    let mut count = 0;
+    let mut previous = vec![];
+
+    for dset in DSets::new(2, n) {
+        for dsym in DSyms::new(&dset) {
+            let code = dsym.to_binary().unwrap();
+            count += 1;
+
+            for i in 0..=code.len() {
+                if i >= code.len()
+                    || i >= previous.len()
+                    || code[i] != previous[i]
+                {
+                    print!(
+                        "{}{}",
+                        str::from_utf8(&[(i + 1) as u8]).unwrap(),
+                        str::from_utf8(&code[i..]).unwrap()
+                    );
+
+                    previous = code;
+                    break;
+                }
+            }
+        }
+    }
+
+    eprintln!("{} symbols generated.", count);
 }
