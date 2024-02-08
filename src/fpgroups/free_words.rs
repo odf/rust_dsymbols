93c24f4219278f7a68a004c732f6a78fc0ddab07
odf <olaf.delgado@gmail.com>
2024-02-08 15:52:23 +1100

    introduce the FreeWord type

--- src/fpgroups/free_words.rs
+++ src/fpgroups/free_words.rs
@@ -18,52 +18,84 @@ fn normalized(ws: &[&[isize]]) -> Vec<isize> {
 }
 
 
-pub fn empty() -> Vec<isize> {
-    vec![]
+#[derive(Debug, Eq, PartialEq)]
+pub struct FreeWord {
+    w: Vec<isize>
 }
 
 
-pub fn word(w: &[isize]) -> Vec<isize> {
-    normalized(&[w])
+impl FreeWord {
+    pub fn new(w: &[isize]) -> Self {
+        Self { w: normalized(&[w]) }
+    }
+
+    pub fn empty() -> Self {
+        Self::new(&[])
+    }
+
+    pub fn inverse(&self) -> Self {
+        Self::new(&self.w.iter().rev().map(|x| -x).collect::<Vec<_>>())
+    }
+
+    pub fn raised_to(&self, m: isize) -> Self {
+        if m < 0 {
+            self.inverse().raised_to(-m)
+        } else {
+            let w: Vec<_> = iter::repeat(&self.w[..])
+                .take(m as usize)
+                .collect();
+            Self::new(&normalized(&w))
+        }
+    }
 }
 
 
-pub fn inverse(w: &[isize]) -> Vec<isize> {
-    word(w).iter().rev().map(|x| -x).collect()
+impl From<&[isize]> for FreeWord {
+    fn from(value: &[isize]) -> Self {
+        Self { w: normalized(&[value]) }
+    }
 }
 
 
-pub fn raised_to(m: isize, w: &[isize]) -> Vec<isize> {
-    if m < 0 {
-        raised_to(-m, &inverse(w))
-    } else {
-        normalized(&iter::repeat(w).take(m as usize).collect::<Vec<_>>())
+impl From<Vec<isize>> for FreeWord {
+    fn from(value: Vec<isize>) -> Self {
+        (&value[..]).into()
     }
 }
 
 
 #[test]
-fn test_fp_words_normalized() {
-    assert_eq!(normalized(&[&[]]), empty());
-    assert_eq!(normalized(&[&[1, 2]]), word(&[1, 2]));
-    assert_eq!(normalized(&[&[1, 2], &[-2, -1]]), empty());
-    assert_eq!(normalized(&[&[1, 2, -2], &[1, 2]]), word(&[1, 1, 2]));
+fn test_freeword_normalized() {
+    assert_eq!(normalized(&[&[]]), &[]);
+    assert_eq!(normalized(&[&[1, 2]]), &[1, 2]);
+    assert_eq!(normalized(&[&[1, 2], &[-2, -1]]), &[]);
+    assert_eq!(normalized(&[&[1, 2, -2], &[1, 2]]), &[1, 1, 2]);
 }
 
 
 #[test]
-fn test_fp_words_inverse() {
-    assert_eq!(inverse(&empty()), empty());
-    assert_eq!(inverse(&[1, 2, 1, -2]), &[2, -1, -2, -1]);
+fn test_freeword_inverse() {
+    assert_eq!(FreeWord::empty().inverse(), FreeWord::empty());
+    assert_eq!(
+        FreeWord::new(&[1, 2, 1, -2]).inverse(),
+        FreeWord::new(&[2, -1, -2, -1])
+    );
 }
 
 
 #[test]
-fn test_fp_words_raise_to() {
-    assert_eq!(raised_to(0, &empty()), empty());
-    assert_eq!(raised_to(-3, &empty()), empty());
-    assert_eq!(raised_to(0, &[1]), empty());
-    assert_eq!(raised_to(1, &[1]), &[1]);
-    assert_eq!(raised_to(-3, &[1]), &[-1, -1, -1]);
-    assert_eq!(raised_to(3, &[1, 2, 1, -2, -1]), &[1, 2, 1, 1, 1, -2, -1]);
+fn test_freeword_raise_to() {
+    assert_eq!(FreeWord::empty().raised_to(0), FreeWord::empty());
+    assert_eq!(FreeWord::empty().raised_to(-3), FreeWord::empty());
+    assert_eq!(FreeWord::new(&[1]).raised_to(0), FreeWord::empty());
+    assert_eq!(FreeWord::new(&[1]).raised_to(1), FreeWord::new(&[1]));
+
+    assert_eq!(
+        FreeWord::new(&[1]).raised_to(-3),
+        FreeWord::new(&[-1, -1, -1])
+    );
+    assert_eq!(
+        FreeWord::new(&[1, 2, 1, -2, -1]).raised_to(3),
+        FreeWord::new(&[1, 2, 1, 1, 1, -2, -1])
+    );
 }
