288f7dd5cd04d5519a330a6aac9d7132b98c8df2
odf <olaf.delgado@gmail.com>
2024-02-13 15:54:37 +1100

    write helpers for coset table (i.e. subgroup) enumeration (untested)

--- src/fpgroups/cosets.rs
+++ src/fpgroups/cosets.rs
@@ -256,6 +256,111 @@ pub fn core_table(base: &CosetTable) -> CosetTable {
 }
 
 
+fn first_free_in_table(table: &DynamicCosetTable) -> Option<(usize, isize)> {
+    for k in 0..=table.top_row() {
+        for g in table.all_gens() {
+            if table.get(k, g).is_none() {
+                return Some((k, g));
+            }
+        }
+    }
+    None
+}
+
+
+fn close_gaps_recursively(
+    rels: &Vec<FreeWord>, table: &mut DynamicCosetTable, index: usize
+)
+    -> bool
+{
+    let mut q = VecDeque::from([index]);
+
+    while let Some(row) = q.pop_front() {
+        for rel in rels {
+            let (head, tail, gap, c) = scan_both_ways(table, rel, row);
+            if gap == 1 {
+                table.join(head, tail, c);
+                q.push_back(head);
+            } else if gap == 0 && head != tail {
+                return false;
+            }
+        }
+    }
+    true
+}
+
+
+fn potential_children(
+    table: &DynamicCosetTable, rels: &Vec<FreeWord>, max_rows: usize
+)
+    -> Vec<DynamicCosetTable>
+{
+    let mut result = vec![];
+
+    if let Some((k, g)) = first_free_in_table(table) {
+        let limit = max_rows.min(table.top_row() + 2);
+        for pos in k..limit {
+            if table.get(pos, -g).is_none() {
+                let mut t = table.clone();
+                t.join(k, pos, g);
+                if close_gaps_recursively(rels, &mut t, k) {
+                    result.push(t);
+                }
+            }
+        }
+    }
+
+    result
+}
+
+
+fn compare_renumbered_from(table: &DynamicCosetTable, start: usize) -> isize {
+    let mut n2o = HashMap::from([(0, start)]);
+    let mut o2n = HashMap::from([(start, 0)]);
+
+    for row in 0..=table.top_row() {
+        assert!(row < n2o.len(), "coset table is not transitive");
+
+        for g in table.all_gens() {
+            if let Some(t) = table.get(n2o[&row], g) {
+                if !o2n.contains_key(&t) {
+                    let n = n2o.len();
+                    o2n.insert(t, n);
+                    n2o.insert(n, t);
+                }
+            }
+
+            let oval = table.get(row, g);
+            let nval = table.get(n2o[&row], g)
+                .and_then(|t| o2n.get(&t)).cloned();
+
+            if oval != nval {
+                if oval.is_none() {
+                    return -1;
+                } else if nval.is_none() {
+                    return 1;
+                } else if nval.unwrap() < oval.unwrap() {
+                    return -1;
+                } else {
+                    return 1;
+                }
+            }
+        }
+    }
+    0
+}
+
+
+fn is_canonical(table: &DynamicCosetTable) -> bool {
+    for start in 0..=table.top_row() {
+        if compare_renumbered_from(table, start) < 0 {
+            return false;
+        }
+    }
+    true
+}
+
+
 #[cfg(test)]
 mod coset_tests {
     use super::*;
