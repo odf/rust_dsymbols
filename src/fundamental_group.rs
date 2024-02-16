616251f0227f208950cb2da92b911191d3c82be9
odf <olaf.delgado@gmail.com>
2024-02-16 17:49:42 +1100

    implement and test inner_edges(), which also test boundary glueing

--- src/fundamental_group.rs
+++ src/fundamental_group.rs
@@ -84,7 +84,7 @@ impl<'a, T: DSym> Boundary<'a, T> {
 }
 
 
-fn spanning_tree<T: DSym>(ds: T) -> Vec<(usize, usize)> {
+fn spanning_tree<T: DSym>(ds: &T) -> Vec<(usize, usize)> {
     let mut seen = HashSet::new();
     let mut result = Vec::new();
 
@@ -102,9 +102,18 @@ fn spanning_tree<T: DSym>(ds: T) -> Vec<(usize, usize)> {
 }
 
 
+pub fn inner_edges<T: DSym>(ds: &T) -> Vec<(usize, usize)> {
+    let tree = spanning_tree(ds);
+    let todo: Vec<_> = tree.iter().map(|&(d, i)| (d, i, i)).collect();
+    let glued = Boundary::new(ds).glue_recursively(todo);
+
+    glued.iter().map(|&(d, i, _)| (d, i)).collect()
+}
+
+
 #[test]
 fn test_spanning_tree() {
-    let tree = |s: &str| spanning_tree(s.parse::<PartialDSym>().unwrap());
+    let tree = |s: &str| spanning_tree(&s.parse::<PartialDSym>().unwrap());
 
     assert_eq!(tree("<1.1:3:1 2 3,1 3,2 3:4 8,3>"), vec![(3, 1), (2, 2)]);
     assert_eq!(tree("<1.1:2 3:2,1 2,1 2,2:6,3 2,6>"), vec![(2, 0)]);
@@ -135,6 +144,44 @@ fn test_spanning_tree() {
             (24, 2), (11, 0), (12, 1), (13, 0), (14, 1), (15, 0), (16, 1),
             (1, 0), (2, 1), (3, 0), (4, 1), (5, 0), (6, 1), (7, 0), (8, 1),
             (9, 0)
+        ]
+    );
+}
+
+
+#[test]
+fn test_inner_edges() {
+    let inner = |s: &str| inner_edges(&s.parse::<PartialDSym>().unwrap());
+
+    assert_eq!(inner("<1.1:3:1 2 3,1 3,2 3:4 8,3>"), vec![(3, 1), (2, 2)]);
+    assert_eq!(inner("<1.1:2 3:2,1 2,1 2,2:6,3 2,6>"), vec![(2, 0)]);
+    assert_eq!(
+        inner("<1.1:8:2 4 6 8,8 3 5 7,1 2 3 4 5 6 7 8:4,4 6 8 4>"),
+        vec![(8, 0), (7, 1), (6, 0), (5, 1), (4, 0), (3, 1), (2, 0), (8, 1)]
+    );
+    assert_eq!(
+        inner("
+            <1.1:12:2 5 7 10 11 12,1 4 6 9 7 12 11,3 5 8 6 11 12 10:4 4,6 3 3>
+        "),
+        vec![
+            (12, 0), (9, 1), (5, 0), (3, 1), (6, 0), (10, 1), (11, 0), (5, 2),
+            (2, 0), (2, 1), (4, 0), (12, 1), (3, 2)
+        ]
+    );
+    assert_eq!(
+        inner("
+            <1.1:24:
+            2 4 6 8 10 12 14 16 18 20 22 24,
+            16 3 5 7 9 11 13 15 24 19 21 23,
+            10 9 20 19 14 13 22 21 24 23 18 17:
+            8 4,3 3 3 3
+            >
+        "),
+        vec![
+            (24,0), (23, 1), (22, 0), (21, 1), (20, 0), (19, 1), (18, 0),
+            (24, 2), (11, 0), (12, 1), (13, 0), (14, 1), (15, 0), (16, 1),
+            (1, 0), (2, 1), (3, 0), (4, 1), (5, 0), (6, 1), (7, 0), (8, 1),
+            (9, 0), (24, 1), (23, 2), (11, 1)
         ]
     );
 }
