32af528cd8612c7e778777182e4942276dc71ce1
odf <olaf.delgado@gmail.com>
2024-02-16 17:08:21 +1100

    make fundamental tree results match old JS code; add more test cases

--- src/fundamental_group.rs
+++ src/fundamental_group.rs
@@ -88,7 +88,8 @@ fn spanning_tree<T: DSym>(ds: T) -> Vec<(usize, usize)> {
     let mut seen = HashSet::new();
     let mut result = Vec::new();
 
-    for (maybe_i, d, di) in ds.full_traversal() {
+    // elements reversal allows for use of old JS results in testing
+    for (maybe_i, d, di) in ds.traversal(0..=ds.dim(), (1..=ds.size()).rev()) {
         if !seen.contains(&di) {
             if let Some(i) = maybe_i {
                 result.push((d, i));
@@ -105,6 +106,21 @@ fn spanning_tree<T: DSym>(ds: T) -> Vec<(usize, usize)> {
 fn test_spanning_tree() {
     let tree = |s: &str| spanning_tree(s.parse::<PartialDSym>().unwrap());
 
+    assert_eq!(tree("<1.1:3:1 2 3,1 3,2 3:4 8,3>"), vec![(3, 1), (2, 2)]);
+    assert_eq!(tree("<1.1:2 3:2,1 2,1 2,2:6,3 2,6>"), vec![(2, 0)]);
+    assert_eq!(
+        tree("<1.1:8:2 4 6 8,8 3 5 7,1 2 3 4 5 6 7 8:4,4 6 8 4>"),
+        vec![(8, 0), (7, 1), (6, 0), (5, 1), (4, 0), (3, 1), (2, 0)]
+    );
+    assert_eq!(
+        tree("
+            <1.1:12:2 5 7 10 11 12,1 4 6 9 7 12 11,3 5 8 6 11 12 10:4 4,6 3 3>
+        "),
+        vec![
+            (12, 0), (9, 1), (5, 0), (3, 1), (6, 0), (10, 1), (11, 0), (5, 2),
+            (2, 0), (2, 1), (4, 0),
+        ]
+    );
     assert_eq!(
         tree("
             <1.1:24:
@@ -113,13 +129,12 @@ fn test_spanning_tree() {
             10 9 20 19 14 13 22 21 24 23 18 17:
             8 4,3 3 3 3
             >
-        ").len(),
-        23
-    );
-    assert_eq!(tree("<1.1:3:1 2 3,1 3,2 3:4 8,3>"), vec![(1, 2), (2, 1)]);
-    assert_eq!(tree("<1.1:2 3:2,1 2,1 2,2:6,3 2,6>"), vec![(1, 0)]);
-    assert_eq!(
-        tree("<1.1:8:2 4 6 8,8 3 5 7,1 2 3 4 5 6 7 8:4,4 6 8 4>"),
-        vec![(1, 0), (2, 1), (3, 0), (4, 1), (5, 0), (6, 1), (7, 0)]
+        "),
+        vec![
+            (24,0), (23, 1), (22, 0), (21, 1), (20, 0), (19, 1), (18, 0),
+            (24, 2), (11, 0), (12, 1), (13, 0), (14, 1), (15, 0), (16, 1),
+            (1, 0), (2, 1), (3, 0), (4, 1), (5, 0), (6, 1), (7, 0), (8, 1),
+            (9, 0)
+        ]
     );
 }
