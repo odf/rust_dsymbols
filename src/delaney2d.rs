60e8cd19f65447a0c7cbde8f57ada81339305cdc
odf <olaf.delgado@gmail.com>
2024-02-22 17:24:14 +1100

    implement is_euclidean(), is_hyperbolic() and is_spherical()

--- src/delaney2d.rs
+++ src/delaney2d.rs
@@ -1,5 +1,7 @@
 use num_rational::Rational64;
+use num_traits::{Signed, Zero};
 
+use crate::derived::oriented_cover;
 use crate::dsyms::*;
 
 
@@ -36,6 +38,35 @@ pub fn curvature<T: DSym>(ds: &T) -> Rational64 {
 }
 
 
+pub fn is_euclidean<T: DSym>(ds: &T) -> bool {
+    curvature(ds).is_zero()
+}
+
+
+pub fn is_hyperbolic<T: DSym>(ds: &T) -> bool {
+    curvature(ds).is_negative()
+}
+
+
+pub fn is_spherical<T: DSym>(ds: &T) -> bool {
+    if !curvature(ds).is_positive() {
+        false
+    } else {
+        let dso = oriented_cover(ds);
+        let cones: Vec<_> = orbit_types_2d(&dso).iter()
+            .map(|&(v, _)| v)
+            .filter(|&v| v.is_some_and(|v| v > 1))
+            .collect();
+
+        match cones.len() {
+            1 => false,
+            2 => cones[0] == cones[1],
+            _ => true,
+        }
+    }
+}
+
+
 #[test]
 fn test_curvature() {
     let curv = |s: &str| curvature(&s.parse::<PartialDSym>().unwrap());
@@ -61,4 +92,21 @@ fn test_curvature() {
                 8 12 16,8 12 16>
             "),
             Rational64::new(-23, 6));
+}
+
+
+#[test]
+fn test_is_spherical() {
+    let passes = |s: &str| is_spherical(&s.parse::<PartialDSym>().unwrap());
+
+    assert!(passes("<1.1:1:1,1,1:3,3>"));
+    assert!(passes("<1.1:1:1,1,1:3,4>"));
+    assert!(passes("<1.1:1:1,1,1:3,5>"));
+    assert!(!passes("<1.1:1:1,1,1:3,6>"));
+    assert!(!passes("<1.1:1:1,1,1:3,7>"));
+
+    assert!(passes("<1.1:2:2,1 2,1 2:2,1 1>"));
+    assert!(!passes("<1.1:2:2,1 2,1 2:2,1 3>"));
+    assert!(passes("<1.1:2:2,1 2,1 2:2,3 3>"));
+    assert!(!passes("<1.1:2:2,1 2,1 2:2,3 4>"));
 }
