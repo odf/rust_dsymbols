f2fe69c5eea425d077689ab653f871e3a9fa44ea
odf <olaf.delgado@gmail.com>
2024-02-27 12:12:03 +1100

    start implementing pseudo-toroidal cover candidate list construction

--- src/delaney3d.rs
+++ src/delaney3d.rs
@@ -1,6 +1,7 @@
+use std::collections::HashMap;
 use std::iter::successors;
 
-use crate::fpgroups::cosets::CosetTable;
+use crate::fpgroups::cosets::{core_table, coset_tables, CosetTable};
 use crate::fpgroups::free_words::FreeWord;
 
 
@@ -57,6 +58,40 @@ fn degree(ct: &CosetTable, w: &FreeWord) -> usize {
 }
 
 
-fn flattens_all(ct: &CosetTable, cones: Vec<(&FreeWord, usize)>) -> bool {
-    cones.iter().all(|&(wd, deg)| degree(ct, wd) == deg)
+fn flattens_all(ct: &CosetTable, cones: &Vec<(FreeWord, usize)>) -> bool {
+    cones.iter().all(|(wd, deg)| degree(ct, wd) == *deg)
+}
+
+
+fn construct_candidates(
+    nr_gens: usize,
+    rels: &Vec<FreeWord>,
+    cones: &Vec<(FreeWord, usize)>,
+)
+    -> HashMap<String, Vec<CosetTable>>
+{
+    let core_tables: Vec<_> = coset_tables(nr_gens, rels, 4)
+        .map(|ct| core_table(&ct))
+        .collect();
+    let cones2: Vec<_> = cones.iter().filter(|(_, d)| *d == 2).cloned().collect();
+    let cones3 = cones.iter().filter(|(_, d)| *d == 3).cloned().collect();
+
+    let mut result = HashMap::new();
+    for p in point_groups() {
+        result.insert(p, vec![]);
+    }
+
+    for table in core_tables.iter() {
+        if flattens_all(table, cones) {
+            result.get_mut(&core_type(&table)).unwrap().push(table.clone());
+        }
+    }
+
+    for ta in core_tables.iter().filter(|ct| flattens_all(ct, &cones3)) {
+        for tb in core_tables.iter().filter(|ct| ct.len() == 2) {
+            //let tx = intersection_table(ta, tb);
+        }
+    }
+
+    result
 }
