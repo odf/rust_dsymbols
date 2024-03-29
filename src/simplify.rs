96b581dee27220a491f5d485e3b8885b7c62976f
odf <olaf.delgado@gmail.com>
2024-03-30 10:55:11 +1100

    test output properties instead of identities

--- src/simplify.rs
+++ src/simplify.rs
@@ -38,7 +38,8 @@ pub fn merge_all<T: DSym>(ds: &T) -> PartialDSym {
 #[cfg(test)]
 mod Test {
     use crate::delaney3d::pseudo_toroidal_cover;
-    use crate::derived::minimal_image;
+    use crate::fpgroups::invariants::abelian_invariants;
+    use crate::fundamental_group::fundamental_group;
 
     use super::*;
 
@@ -48,12 +49,39 @@ mod Test {
             let ds = s.parse::<PartialDSym>().unwrap();
             eprintln!("{ds}");
             let cov = pseudo_toroidal_cover(&ds).unwrap();
-            let out = minimal_image(&merge_all(&cov));
-            assert_eq!(out.to_string(), "<1.1:1 3:1,1,1,1:4,3,4>");
+            let out = merge_all(&cov);
+
+            assert_eq!(out.orbit_reps([0, 1, 2], 1..out.size()).len(), 1);
+            assert_eq!(out.orbit_reps([1, 2, 3], 1..out.size()).len(), 1);
+
+            let reps = out.orbit_reps([2, 3], 1..out.size());
+            assert!(reps.iter().all(|&d| out.r(2, 3, d) != Some(2)));
+            let reps = out.orbit_reps([0, 1], 1..out.size());
+            assert!(reps.iter().all(|&d| out.r(0, 1, d) != Some(2)));
+
+            let fg = fundamental_group(&out);
+            let nr_gens = fg.gen_to_edge.len();
+            let rels: Vec<_> = fg.relators.iter().cloned().collect();
+            let inv = abelian_invariants(nr_gens, &rels);
+            assert_eq!(inv, vec![0, 0, 0]);
         };
 
-        test("<1.1:1 3:1,1,1,1:4,3,4>");
-        test("<1.1:3 3:1 2 3,1 3,2 3,1 2 3:3 4,3,4 6>");
-        test("<1.1:4 3:1 2 3 4,1 2 4,1 3 4,2 3 4:3 3 8,4 3,3 4>");
+        test("<1.4:1 3:1,1,1,1:4,3,4>");
+        test("<2.1:2 3:1 2,1 2,1 2,2:3 3,3 4,4>");
+        test("<513.5:2 3:2,1 2,1 2,2:4,2 4,6>");
+        test("<513.8:2 3:2,1 2,1 2,2:6,2 3,6>");
+        test("<3.3:3 3:1 2 3,1 2 3,1 3,2 3:3 3 4,4 4,3>");
+        test("<167.3:3 3:1 2 3,1 3,2 3,1 2 3:3 4,3,4 6>");
+        test("<184.4:3 3:1 2 3,1 3,2 3,1 3:4 6,3,3>");
+        test("<23.14:4 3:1 2 3 4,1 2 4,1 3 4,2 3 4:3 3 8,4 3,3 4>");
+        test("<71.3:4 3:1 2 3 4,1 2 4,1 3 4,2 4:3 3 6,3 3,4>");
+        test("<514.7:4 3:2 4,1 2 3 4,1 2 3 4,3 4:4 4,2 4 4 3,4 4>");
+        test("<553.3:4 3:2 4,1 2 3 4,3 4,2 4:4 6,2 6,4>");
+        test("<45.2:5 3:1 2 3 5,1 2 4 5,1 3 4 5,2 3 4 5:3 3 3,3 3 3,6 4 4>");
+        test("<45.7:5 3:1 2 3 5,1 2 4 5,1 3 4 5,2 3 4 5:3 3 3,4 3 3,6 3 3>");
+        test("<45.12:5 3:1 2 3 5,1 2 4 5,1 3 4 5,2 3 4 5:3 3 6,4 3 3,3 4 4>");
+        test("<54.2:5 3:1 2 3 5,1 2 4 5,1 3 5,2 3 4 5:3 3 3,3 4,3 6>");
+        test("<54.4:5 3:1 2 3 5,1 2 4 5,1 3 5,2 3 4 5:3 3 3,4 4,3 4>");
+        test("<222.77:5 3:1 2 4 5,1 3 5,2 3 4 5,1 5 4:4 12,3 2,3 4>");
     }
 }
