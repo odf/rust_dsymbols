94dd746d7f7a0bc449fdbd68bbe80426207472f2
odf <olaf.delgado@gmail.com>
2024-04-25 14:38:26 +1000

    include index of D-symbol in file into the output

--- examples/test_euclidicity.rs
+++ examples/test_euclidicity.rs
@@ -6,15 +6,17 @@ use rust_dsymbols::euclidicity::Euclidean::*;
 
 
 fn main() {
+    let mut i = 0;
     for line in stdin().lines() {
+        i += 1;
+
         if let Some(ds) = line.ok().and_then(|s|
             s.parse::<PartialDSym>().ok()
         ) {
-            println!("{ds}");
             match is_euclidean(&ds) {
-                Yes => println!("good"),
-                No(s) => println!("bad: {s}"),
-                Maybe(s, _) => println!("undecided: {s}"),
+                Yes => println!("{i} good"),
+                No(s) => println!("{i} bad: {s}"),
+                Maybe(s, _) => println!("{i} undecided: {s}"),
             }
         }
     }
