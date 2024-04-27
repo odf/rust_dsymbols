a5e7ca979d26114bb6bcccb257553de76ade27bd
odf <olaf.delgado@gmail.com>
2024-04-28 09:50:43 +1000

    add profiling support

--- examples/test_euclidicity.rs
+++ examples/test_euclidicity.rs
@@ -6,6 +6,12 @@ use rust_dsymbols::euclidicity::Euclidean::*;
 
 
 fn main() {
+    run();
+}
+
+
+#[cfg(not(feature = "pprof"))]
+fn run() {
     let mut i = 0;
     for line in stdin().lines() {
         i += 1;
@@ -25,4 +31,38 @@ fn main() {
             }
         }
     }
+}
+
+
+#[cfg(feature = "pprof")]
+fn run() {
+    let guard = pprof::ProfilerGuardBuilder::default()
+        .frequency(1000)
+        .blocklist(&["libc", "libgcc", "pthread", "vdso"])
+        .build().unwrap();
+
+    let mut i = 0;
+    for line in stdin().lines() {
+        i += 1;
+
+        if let Some(ds) = line.ok().and_then(|s|
+            s.parse::<PartialDSym>().ok()
+        ) {
+            match is_euclidean(&ds) {
+                Yes => {
+                    println!(
+                        "#Symbol {i} is good: simplified cover recognized"
+                    );
+                    println!("{ds}");
+                },
+                No(s) => println!("#Symbol {i} is bad: {s}"),
+                Maybe(s, _) => println!("#Symbol {i} is undecided: {s}"),
+            }
+        }
+    }
+
+    if let Ok(report) = guard.report().build() {
+        let file = std::fs::File::create("flamegraph.svg").unwrap();
+        report.flamegraph(file).unwrap();
+    };
 }
