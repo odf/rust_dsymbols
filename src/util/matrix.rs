c0370587cf95988bdce2308f78953d9f282c5325
odf <olaf.delgado@gmail.com>
2024-07-29 17:42:28 +1000

    add code for converting things into matrices

--- src/util/matrix.rs
+++ src/util/matrix.rs
@@ -30,15 +30,43 @@ impl<T: Scalar, const N: usize, const M: usize> Matrix<T, N, M> {
 }
 
 
+trait AsMatrix<T, const N: usize, const M: usize> {
+    fn matrix(self) -> Matrix<T, N, M>;
+}
+
+
+impl<T: Scalar, const N: usize, const M: usize>
+    AsMatrix<T, N, M> for [[T; M]; N]
+{
+    fn matrix(self) -> Matrix<T, N, M> {
+        Matrix::new(self)
+    }
+}
+
+
+impl<T: Scalar, const M: usize> AsMatrix<T, 1, M> for [T; M] {
+    fn matrix(self) -> Matrix<T, 1, M> {
+        Matrix::new([self])
+    }
+}
+
+
+impl<T: Scalar> AsMatrix<T, 1, 1> for T {
+    fn matrix(self) -> Matrix<T, 1, 1> {
+        Matrix::new([[self]])
+    }
+}
+
+
 impl<T: Scalar> Matrix<T, 1, 1> {
-    fn as_scalar(self) -> T {
+    fn scalar(self) -> T {
         self.data[0][0]
     }
 }
 
 
 impl<T: Scalar, const M: usize> Matrix<T, 1, M> {
-    fn as_vector(self) -> [T; M] {
+    fn vector(self) -> [T; M] {
         self.data[0]
     }
 }
@@ -84,19 +112,19 @@ impl Scalar for f64 {}
 #[test]
 fn test_matrix_mul() {
     assert_eq!(
-        (Matrix::new([[1, 2, 3]]) * [[3], [2], [1]]).as_scalar(),
+        (Matrix::new([[1, 2, 3]]) * [[3], [2], [1]]).scalar(),
         10
     );
     assert_eq!(
-        Matrix::new([[1, 2, 3]]) * Matrix::new([[3, 2, 1]]).transpose(),
-        Matrix::new([[10]])
+        [1, 2, 3].matrix() * [3, 2, 1].matrix().transpose(),
+        10.matrix()
     );
     assert_eq!(
         Matrix::new([[1.0, 1.0], [0.0, 1.0]]) * [[1.0, 2.0], [0.0, 1.0]],
         Matrix::new([[1.0, 3.0], [0.0, 1.0]])
     );
     assert_eq!(
-        (Matrix::new([[1.0, 1.0]]) * [[1.0, 2.0], [0.0, 1.0]]).as_vector(),
+        (Matrix::new([[1.0, 1.0]]) * [[1.0, 2.0], [0.0, 1.0]]).vector(),
         [1.0, 3.0]
     );
 }
