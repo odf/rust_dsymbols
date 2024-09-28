25376fb20e37eab823e8468e8856e44696d59034
odf <olaf.delgado@gmail.com>
2024-09-28 14:10:09 +1000

    add and revise functionality; add tests

--- src/util/vec_matrix.rs
+++ src/util/vec_matrix.rs
@@ -19,35 +19,13 @@ pub struct VecMatrix<T> {
 }
 
 
-impl<T: Scalar + Clone> VecMatrix<T> {
+impl<T: Scalar> VecMatrix<T> {
     pub fn nr_rows(&self) -> usize {
         self.nr_rows
     }
 
     pub fn nr_columns(&self) -> usize {
         self.nr_cols
-    }
-}
-
-
-impl<T: Scalar> Index<(usize, usize)> for VecMatrix<T>
-{
-    type Output = T;
-
-    fn index(&self, index: (usize, usize)) -> &Self::Output {
-        assert!(index.0 < self.nr_rows);
-        assert!(index.1 < self.nr_cols);
-        &self.data[index.0 * self.nr_cols + index.1]
-    }
-}
-
-
-impl<T: Scalar> IndexMut<(usize, usize)> for VecMatrix<T>
-{
-    fn index_mut(&mut self, index: (usize, usize)) -> &mut T {
-        assert!(index.0 < self.nr_rows);
-        assert!(index.1 < self.nr_cols);
-        &mut self.data[index.0 * self.nr_cols + index.1]
     }
 }
 
@@ -68,6 +46,45 @@ impl<T: Scalar> IndexMut<usize> for VecMatrix<T>
     fn index_mut(&mut self, index: usize) -> &mut [T] {
         assert!(index < self.nr_rows);
         &mut self.data[index * self.nr_cols .. (index + 1) * self.nr_cols]
+    }
+}
+
+
+impl<T: Scalar + Clone, const N: usize, const M: usize>
+    From<[[T; M]; N]> for VecMatrix<T>
+{
+    fn from(data: [[T; M]; N]) -> Self {
+        let mut result = VecMatrix::new(N, M);
+
+        for i in 0..N {
+            for j in 0..M {
+                result[i][j] = data[i][j].clone();
+            }
+        }
+
+        result
+    }
+}
+
+
+impl<T: Scalar + Clone, const M: usize> From<[T; M]> for VecMatrix<T> {
+    fn from(data: [T; M]) -> Self {
+        let mut result = VecMatrix::new(1, M);
+
+        for j in 0..M {
+            result[0][j] = data[j].clone();
+        }
+
+        result
+    }
+}
+
+
+impl<T: Scalar + Clone> From<T> for VecMatrix<T> {
+    fn from(data: T) -> Self {
+        let mut result = VecMatrix::new(1, 1);
+        result[0][0] = data.clone();
+        result
     }
 }
 
@@ -158,4 +175,169 @@ impl<T: Scalar + Clone> VecMatrix<T> {
 
         result
     }
+}
+
+
+impl<T: Scalar + Copy> Mul<VecMatrix<T>> for VecMatrix<T> {
+    type Output = VecMatrix<T>;
+
+    fn mul(self, rhs: VecMatrix<T>) -> Self::Output {
+        let mut result = VecMatrix::new(self.nr_rows, rhs.nr_cols);
+
+        for i in 0..self.nr_rows {
+            for j in 0..rhs.nr_cols {
+                let mut x = T::zero();
+                for k in 0..self.nr_cols {
+                    x = x + self[i][k] * rhs[k][j];
+                }
+                result[i][j] = x;
+            }
+        }
+
+        result
+    }
+}
+
+
+impl<T: Scalar + Copy, const N: usize, const M: usize>
+    Mul<VecMatrix<T>> for [[T; M]; N]
+{
+    type Output = VecMatrix<T>;
+
+    fn mul(self, rhs: VecMatrix<T>) -> Self::Output {
+        VecMatrix::from(self) * rhs
+    }
+}
+
+
+impl<T: Scalar + Copy, const M: usize, const L: usize>
+    Mul<[[T; L]; M]> for VecMatrix<T>
+{
+    type Output = VecMatrix<T>;
+
+    fn mul(self, rhs: [[T; L]; M]) -> Self::Output {
+        self * VecMatrix::from(rhs)
+    }
+}
+
+
+impl Mul<VecMatrix<i64>> for i64 {
+    type Output = VecMatrix<i64>;
+
+    fn mul(self, rhs: VecMatrix<i64>) -> Self::Output {
+        let mut result = VecMatrix::new(rhs.nr_rows, rhs.nr_cols);
+
+        for i in 0..rhs.nr_rows {
+            for j in 0..rhs.nr_cols {
+                result[i][j] = self * rhs[i][j];
+            }
+        }
+
+        result
+    }
+}
+
+
+impl Mul<VecMatrix<f64>> for f64 {
+    type Output = VecMatrix<f64>;
+
+    fn mul(self, rhs: VecMatrix<f64>) -> Self::Output {
+        let mut result = VecMatrix::new(rhs.nr_rows, rhs.nr_cols);
+
+        for i in 0..rhs.nr_rows {
+            for j in 0..rhs.nr_cols {
+                result[i][j] = self * rhs[i][j];
+            }
+        }
+
+        result
+    }
+}
+
+
+impl<T: Scalar + Copy> Mul<T> for VecMatrix<T> {
+    type Output = Self;
+
+    fn mul(self, rhs: T) -> Self::Output {
+        let mut result = VecMatrix::new(self.nr_rows, self.nr_cols);
+
+        for i in 0..self.nr_rows {
+            for j in 0..self.nr_cols {
+                result[i][j] = self[i][j] * rhs;
+            }
+        }
+
+        result
+    }
+}
+
+
+#[test]
+fn test_matrix_indexing() {
+    let mut m = VecMatrix::from([[1.0, 1.0], [0.0, 1.0]]);
+
+    m.set_row(0, m.get_row(0) * 3.0);
+    m[1][0] = m[1][0] + 4.0;
+
+    assert_eq!(m, [[3.0, 3.0], [4.0, 1.0]].into());
+}
+
+
+#[test]
+fn test_matrix_submatrix() {
+    assert_eq!(
+        VecMatrix::from([[1, 2, 3], [4, 5, 6], [7, 8, 9]]).submatrix(0..2, [0, 2]),
+        VecMatrix::from([[1, 3], [4, 6]])
+    )
+}
+
+
+#[test]
+fn test_matrix_mul() {
+    assert_eq!(
+        (VecMatrix::from([[1, 2, 3]]) * [[3], [2], [1]])[0][0],
+        10
+    );
+    assert_eq!(
+        VecMatrix::from([1, 2, 3]) * VecMatrix::from([3, 2, 1]).transpose(),
+        10.into()
+    );
+    assert_eq!(
+        [[1, 2, 3]] * VecMatrix::from([3, 2, 1]).transpose(),
+        [[10]].into()
+    );
+    assert_eq!(
+        VecMatrix::from([[1.0, 1.0], [0.0, 1.0]]) * [[1.0, 2.0], [0.0, 1.0]],
+        [[1.0, 3.0], [0.0, 1.0]].into()
+    );
+    assert_eq!(
+        (
+            VecMatrix::from([[1.0, 1.0], [0.0, 1.0]]) *
+            [[1.0, 2.0], [0.0, 1.0]]
+        )[0][1],
+        3.0
+    );
+    assert_eq!(
+        (
+            VecMatrix::from([[1.0, 1.0], [0.0, 1.0]]) *
+            [[1.0, 2.0], [0.0, 1.0]]
+        )[0],
+        [1.0, 3.0]
+    );
+    assert_eq!(
+        (VecMatrix::from([[1.0, 1.0]]) * [[1.0, 2.0], [0.0, 1.0]])[0],
+        [1.0, 3.0]
+    );
+    assert_eq!(
+        VecMatrix::from([[1, 2], [3, 4]]) * 2,
+        VecMatrix::from([[2, 4], [6, 8]])
+    );
+    assert_eq!(
+        VecMatrix::from([[1.0, 2.0], [3.0, 4.0]]) * 2.0,
+        VecMatrix::from([[2.0, 4.0], [6.0, 8.0]])
+    );
+    assert_eq!(
+        2.5 * VecMatrix::from([[1.0, 2.0], [3.0, 4.0]]),
+        VecMatrix::from([[2.5, 5.0], [7.5, 10.0]])
+    );
 }
