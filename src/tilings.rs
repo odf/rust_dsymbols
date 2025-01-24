065ac290ab977e226375e719cf1aad5bcd5f8cd4
odf <olaf.delgado@gmail.com>
2025-01-24 16:18:21 +1100

    implement corner shifts (untested)

--- src/tilings.rs
+++ src/tilings.rs
@@ -6,8 +6,10 @@ use crate::fundamental_group::fundamental_group;
 use crate::geometry::vec_matrix::VecMatrix;
 
 
-fn edge_translations<T: DSym>(cov: &T)
-    -> BTreeMap<(usize, usize), VecMatrix<i64>>
+type EdgeVectors = BTreeMap<(usize, usize), VecMatrix<i64>>;
+
+
+fn edge_translations<T: DSym>(cov: &T) -> EdgeVectors
 {
     let fg = fundamental_group(cov);
     let nr_gens = fg.nr_generators();
@@ -26,17 +28,38 @@ fn edge_translations<T: DSym>(cov: &T)
 }
 
 
+fn corner_shifts<T: DSym>(cov: &T, e2t: EdgeVectors) -> EdgeVectors {
+    let zero = VecMatrix::new(3, 1);
+
+    let mut result = BTreeMap::new();
+
+    for i in 0..=cov.dim() {
+        let idcs = (0..=cov.dim()).filter(|&k| k != i);
+        for (maybe_k, d, dk) in cov.traversal(idcs, 0..=cov.size()) {
+            let shift = if let Some(k) = maybe_k {
+                result.get(&(d, i)).unwrap() - e2t.get(&(d, k)).unwrap_or(&zero)
+            } else {
+                zero.clone()
+            };
+            result.insert((dk, i), shift);
+        }
+    }
+
+    result
+}
+
+
 #[test]
 fn test_edge_translations() {
     use crate::dsets::DSet;
     use crate::delaney3d::pseudo_toroidal_cover;
     use crate::dsyms::PartialDSym;
-    
+
     fn test(spec: &str) {
         let ds = spec.parse::<PartialDSym>().unwrap();
         let cov = pseudo_toroidal_cover(&ds).unwrap();
         let e2t = edge_translations(&cov);
-        
+
         for i in 0..=cov.dim() {
             for j in i..=cov.dim() {
                 for d in cov.orbit_reps([i, j], 1..=cov.size()) {
@@ -49,12 +72,12 @@ fn test_edge_translations() {
                         }
                         e = cov.op(k, e).unwrap();
                         k = i + j - k;
-        
+
                         if e == d && k == i {
                             break;
                         }
                     }
-        
+
                     assert!(s.is_zero());
                 }
             }
@@ -65,4 +88,4 @@ fn test_edge_translations() {
     test("<1.1:2 3:2,1 2,1 2,2:6,3 2,6>");
     test("<1.1:2 3:1 2,1 2,1 2,2:3 3,3 4,4>");
     test("<1.1:6 3:2 4 6,1 2 3 5 6,3 4 5 6,2 3 4 5 6:6 4,2 3 3,8 4 4>");
-  }
+}
