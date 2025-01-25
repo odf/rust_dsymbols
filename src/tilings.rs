d326381c278d1cfcb178c3dfba9f835584ce2200
odf <olaf.delgado@gmail.com>
2025-01-25 12:06:12 +1100

    add a unit test for corner_shifts()

--- src/tilings.rs
+++ src/tilings.rs
@@ -28,14 +28,14 @@ fn edge_translations<T: DSym>(cov: &T) -> EdgeVectors
 }
 
 
-fn corner_shifts<T: DSym>(cov: &T, e2t: EdgeVectors) -> EdgeVectors {
+fn corner_shifts<T: DSym>(cov: &T, e2t: &EdgeVectors) -> EdgeVectors {
     let zero = VecMatrix::new(3, 1);
 
     let mut result = BTreeMap::new();
 
-    for i in 0..=cov.dim() {
-        let idcs = (0..=cov.dim()).filter(|&k| k != i);
-        for (maybe_k, d, dk) in cov.traversal(idcs, 0..=cov.size()) {
+    for i in cov.indices() {
+        let idcs = cov.indices().filter(|&k| k != i);
+        for (maybe_k, d, dk) in cov.traversal(idcs, cov.elements()) {
             let shift = if let Some(k) = maybe_k {
                 result.get(&(d, i)).unwrap() - e2t.get(&(d, k)).unwrap_or(&zero)
             } else {
@@ -49,43 +49,79 @@ fn corner_shifts<T: DSym>(cov: &T, e2t: EdgeVectors) -> EdgeVectors {
 }
 
 
-#[test]
-fn test_edge_translations() {
+mod test {
     use crate::dsets::DSet;
     use crate::delaney3d::pseudo_toroidal_cover;
     use crate::dsyms::PartialDSym;
 
-    fn test(spec: &str) {
-        let ds = spec.parse::<PartialDSym>().unwrap();
-        let cov = pseudo_toroidal_cover(&ds).unwrap();
-        let e2t = edge_translations(&cov);
+    use super::*;
 
-        for i in 0..=cov.dim() {
-            for j in i..=cov.dim() {
-                for d in cov.orbit_reps([i, j], 1..=cov.size()) {
-                    let mut s = VecMatrix::new(3, 1);
-                    let mut e = d;
-                    let mut k = i;
-                    loop {
-                        if let Some(v) = e2t.get(&(e, k)) {
-                            s = s + v;
-                        }
-                        e = cov.op(k, e).unwrap();
-                        k = i + j - k;
+    #[test]
+    fn test_edge_translations() {
+        fn test(spec: &str) {
+            let ds = spec.parse::<PartialDSym>().unwrap();
+            let cov = pseudo_toroidal_cover(&ds).unwrap();
+            let e2t = edge_translations(&cov);
 
-                        if e == d && k == i {
-                            break;
+            for i in 0..=cov.dim() {
+                for j in i..=cov.dim() {
+                    for d in cov.orbit_reps([i, j], 1..=cov.size()) {
+                        let mut s = VecMatrix::new(3, 1);
+                        let mut e = d;
+                        let mut k = i;
+                        loop {
+                            if let Some(v) = e2t.get(&(e, k)) {
+                                s = s + v;
+                            }
+                            e = cov.op(k, e).unwrap();
+                            k = i + j - k;
+
+                            if e == d && k == i {
+                                break;
+                            }
                         }
+
+                        assert!(s.is_zero());
                     }
-
-                    assert!(s.is_zero());
                 }
             }
         }
+
+        test("<1.1:1 3:1,1,1,1:4,3,4>");
+        test("<1.1:2 3:2,1 2,1 2,2:6,3 2,6>");
+        test("<1.1:2 3:1 2,1 2,1 2,2:3 3,3 4,4>");
+        test("<1.1:6 3:2 4 6,1 2 3 5 6,3 4 5 6,2 3 4 5 6:6 4,2 3 3,8 4 4>");
     }
 
-    test("<1.1:1 3:1,1,1,1:4,3,4>");
-    test("<1.1:2 3:2,1 2,1 2,2:6,3 2,6>");
-    test("<1.1:2 3:1 2,1 2,1 2,2:3 3,3 4,4>");
-    test("<1.1:6 3:2 4 6,1 2 3 5 6,3 4 5 6,2 3 4 5 6:6 4,2 3 3,8 4 4>");
+
+    #[test]
+    fn test_corner_shifts() {
+        fn test(spec: &str) {
+            let ds = spec.parse::<PartialDSym>().unwrap();
+            let cov = pseudo_toroidal_cover(&ds).unwrap();
+            let e2t = edge_translations(&cov);
+            let c2s = corner_shifts(&cov, &e2t);
+
+            for i in cov.indices() {
+                for j in cov.indices() {
+                    if j != i {
+                        for d in cov.elements() {
+                            let sd = &c2s[&(d, i)];
+                            let sdj = &c2s[&(cov.op(j, d).unwrap(), i)];
+                            if let Some(t) = e2t.get(&(d, j)) {
+                                assert_eq!(sd, &(sdj + t));
+                            } else {
+                                assert_eq!(sd, sdj);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        test("<1.1:1 3:1,1,1,1:4,3,4>");
+        test("<1.1:2 3:2,1 2,1 2,2:6,3 2,6>");
+        test("<1.1:2 3:1 2,1 2,1 2,2:3 3,3 4,4>");
+        test("<1.1:6 3:2 4 6,1 2 3 5 6,3 4 5 6,2 3 4 5 6:6 4,2 3 3,8 4 4>");
+    }
 }
