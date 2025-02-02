e1d4bb711200e6dae303d530fd342fa18780e258
odf <olaf.delgado@gmail.com>
2025-02-02 16:52:49 +1100

    implement arithmetic operations

--- src/geometry/prime_residue_classes.rs
+++ src/geometry/prime_residue_classes.rs
@@ -1,5 +1,9 @@
-use std::ops::Add;
+use std::ops::{Add, Div, Mul, Neg, Sub};
 
+use num_traits::{One, Zero};
+
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
 struct PrimeResidueClass<const P: i64> {
     value: i64
 }
@@ -19,6 +23,21 @@ impl<const P: i64> PrimeResidueClass<P> {
             }
             true
         }
+    }
+
+    fn inverse(self) -> Self {
+        let (mut t, mut t1) = (0, 1);
+        let (mut r, mut r1) = (P, self.value);
+
+        while r1 != 0 {
+            let q = r / r1;
+            (t, t1) = (t1, t - q * t1);
+            (r, r1) = (r1, r - q * r1);
+        }
+
+        assert_eq!(r, 1);
+
+        t.into()
     }
 }
 
@@ -41,6 +60,64 @@ impl<const P: i64> Add<PrimeResidueClass<P>> for PrimeResidueClass<P> {
 }
 
 
+impl<const P: i64> Sub<PrimeResidueClass<P>> for PrimeResidueClass<P> {
+    type Output = Self;
+
+    fn sub(self, rhs: PrimeResidueClass<P>) -> Self::Output {
+        (self.value - rhs.value).into()
+    }
+}
+
+
+impl<const P: i64> Mul<PrimeResidueClass<P>> for PrimeResidueClass<P> {
+    type Output = Self;
+
+    fn mul(self, rhs: PrimeResidueClass<P>) -> Self::Output {
+        (self.value * rhs.value).into()
+    }
+}
+
+
+impl<const P: i64> Div<PrimeResidueClass<P>> for PrimeResidueClass<P> {
+    type Output = Self;
+
+    fn div(self, rhs: PrimeResidueClass<P>) -> Self::Output {
+        self * rhs.inverse()
+    }
+}
+
+
+impl<const P: i64> Neg for PrimeResidueClass<P> {
+    type Output = Self;
+
+    fn neg(self) -> Self::Output {
+        (-self.value).into()
+    }
+}
+
+
+impl<const P: i64> Zero for PrimeResidueClass<P> {
+    fn zero() -> Self {
+        0.into()
+    }
+
+    fn is_zero(&self) -> bool {
+        self.value == 0
+    }
+}
+
+
+impl<const P: i64> One for PrimeResidueClass<P> {
+    fn one() -> Self {
+        1.into()
+    }
+
+    fn is_one(&self) -> bool {
+        self.value == 1
+    }
+}
+
+
 #[test]
 fn test_valid() {
     assert!(PrimeResidueClass::<2>::valid());
@@ -56,4 +133,18 @@ fn test_invalid() {
     assert!(!PrimeResidueClass::<77>::valid());
     assert!(!PrimeResidueClass::<9999992>::valid());
     assert!(!PrimeResidueClass::<3037000507>::valid()); // prime, but too large
+}
+
+
+#[test]
+fn test_add_sub() {
+    let a = PrimeResidueClass::<61>::from(54);
+    assert_eq!(a + 47.into() - 47.into(), a);
+}
+
+
+#[test]
+fn test_div_mul() {
+    let a = PrimeResidueClass::<61>::from(54);
+    assert_eq!(a * 47.into() / 47.into(), a);
 }
